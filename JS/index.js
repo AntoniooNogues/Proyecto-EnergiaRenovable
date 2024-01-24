e8c59f2ad4debd416aeb91b959f3fbe8df8fde9d
Antonio Nogues <anoguesgomez@safareyes.es>
2024-01-24 17:41:34 +0100

    24/01/2024 V1

---
+++ JS/index.js
@@ -0,0 +1,12 @@
+function mostrarContrasena() {
+    var contrasenaInput = document.getElementById("contrasena");
+    var mostrarContrasenaBtn = document.getElementById("mostrarContrasenaBtn");
+
+    if (contrasenaInput.type === "password") {
+        contrasenaInput.type = "text";
+        mostrarContrasenaBtn.textContent = "Ocultar";
+    } else {
+        contrasenaInput.type = "password";
+        mostrarContrasenaBtn.textContent = "Mostrar";
+    }
+}
